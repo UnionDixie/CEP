547136ae7e389c1d90f2b27ea895e0fc2edf9f0c
Mixie <97407744+Maxi741cv@users.noreply.github.com>
2023-12-03 15:51:26 +0300

    Done first Lab about taylor

--- VSU_NumericalAlghorithms/Numerical_Algorithms_CPP/Lab1_Taylor/main.cpp
+++ VSU_NumericalAlghorithms/Numerical_Algorithms_CPP/Lab1_Taylor/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <cmath>
+#include <numbers>
+#include <iomanip>
+
+// TAYLOR SIN(X)
 
 float y_func(const float x)
 {
@@ -10,14 +14,37 @@ int main()
 {
 	using namespace std;
 
-	const int degree = 30;
+	const int x = 30;
 	const float eps = 1e-5;
 
-	cout << "Degree:" << degree << ", "
+	cout << "Degree:" << x << ", "
 		<< "EPS:" << eps << ", "
 		<< "Function:sin(x)" << "\n"; 
 
-	const float degree_radian = (degree * M_PI) / 180.0f;
+	const float x_radian = (x * std::numbers::pi_v<float>) / 180.0f;
+	cout << "Degree_rad:" << x_radian << "\n";
+
+	float sum = x_radian;
+	float t = x_radian;
+	float d = 3.0f;
+	float xsq = x_radian * x_radian;
+	int i = 1;
+
+	//sin(x)=sigma x-x^3/3! -  ....
+
+	while (abs(t) >= eps) {
+		t = -t * xsq / (d * (d - 1.0));
+		sum += t;
+		d += 2;
+		i++;
+	}
+
+
+	std::cout <<
+		std::fixed << std::setprecision(5) << 
+		sum << " " << 
+		i << " " << 
+		y_func(x_radian) << "\n";
 
 
 
